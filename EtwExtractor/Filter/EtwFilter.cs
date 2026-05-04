e78605b68262170faa79444666e850c023be40ce
Guneet <guberoi@icloud.com>
2026-05-04 15:34:59 -0400

    Filter done

---
+++ EtwExtractor/Filter/EtwFilter.cs
@@ -0,0 +1,28 @@
+﻿using EtwExtractor.Options;
+using Microsoft.Diagnostics.Tracing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EtwExtractor.Filter
+{
+    public class EtwFilter : IFilter<TraceEvent>
+    {
+        private readonly EtwOptions options;
+
+        public EtwFilter(EtwOptions option)
+        {
+            options = option;
+        }
+
+        public bool ShouldKeep(TraceEvent evt)
+        {
+            return ValidateName(evt);
+        }
+
+        private bool ValidateName(TraceEvent evt)
+        {
+            return options.CheckForApplicationName(evt.ProcessName);
+        }
+    }
+}
--- EtwExtractor/Program.cs
+++ EtwExtractor/Program.cs
@@ -4,19 +4,23 @@ using Microsoft.Diagnostics.Tracing.Session;
 using Microsoft.Diagnostics.Tracing.Parsers;
 
 using EtwExtractor.Options;
+using EtwExtractor.Filter;
 
-var EtwTracerOptions = new EtwOptionbuilder()
+var etwTracerOptions = new EtwOptionbuilder()
     .EnableKernelNetworkTCPIP()
     .EnableKernelContextSwitch()
-    .AddApplication("chrome")
+    .AddApplication("Discord")
     .Build();
+
+var etwFilter = new EtwFilter(etwTracerOptions);
 
 using (var session = new TraceEventSession("KernelProcessSession"))
 {
-    session.EnableKernelProvider(EtwTracerOptions.GetKernelOptions());
+
+    session.EnableKernelProvider(etwTracerOptions.GetKernelOptions());
     session.Source.Kernel.All += (data) =>
     {
-        if (EtwTracerOptions.CheckForApplicationName(data.ProcessName))
+        if (etwFilter.ShouldKeep(data))
         {
             Console.WriteLine($"{data.EventName}\t{data.FormattedMessage}\t{data.KernelTime}\t{data.Level}\t{data.ProcessID}\t{data.ProcessName}\t{data.ProviderGuid}\t{data.ProviderName}\t");
         }
